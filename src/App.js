f266efb9265504495be7cd686017ddda53831791
Nabaraj <nabaraj.dhl77@gmail.com>
2022-06-24 21:14:53 +0545

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import TourCard from "./Components/TourCard.js";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import SearchAppBar from "./Components/AppBar";
 function App() {
   return (
     <div className="App">
+      <SearchAppBar />
       <Container>
         <Grid container spacing={3}>
           <TourCard />
--- src/Components/TourCard.js
+++ src/Components/TourCard.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import { AccessTime } from "@mui/icons-material";
-
+import { createTheme, ThemeProvider } from "@mui/system";
 import "./TourCard.css";
-const TourCard = ({ img }) => {
-  console.log(img);
+
+//ADDING CUSTOM THEMES
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 12,
+          },
+        },
+      ],
+    },
+  },
+});
+
+const TourCard = () => {
   return (
     <>
       <Grid item xs={3}>
@@ -17,6 +36,7 @@ const TourCard = ({ img }) => {
             src="https://www.niagarafallsstatepark.com/~/media/parks/niagara-falls/homepage/niagara-falls-mist-880x592.jpg?mw=640&hash=A8A122619847D8A9F7B6395285F6D82CC1E6F438"
             alt="Niagara Falls"
           />
+
           <Box paddingX={1}>
             <Typography variant="subtitle1" component="h2">
               Immerse into the Falls
@@ -33,7 +53,7 @@ const TourCard = ({ img }) => {
                   marginRight: 0.5,
                 }}
               />
-              <Typography variant="caption" component="p">
+              <Typography variant="body2" component="p">
                 5 hours ago
               </Typography>
             </Box>
@@ -42,20 +62,32 @@ const TourCard = ({ img }) => {
                 display: "flex",
                 alignItems: "center",
               }}
+              marginTop={3}
             >
-              <Rating name="read-only" value={4} readOnly />
+              <Rating
+                name="read-only"
+                value={4.5}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
               <Typography
-                variant="body1"
+                variant="body2"
                 component="span"
                 sx={{
                   paddingRight: 1,
                   paddingLeft: 0.4,
                 }}
               >
-                4
+                4.5
               </Typography>
-              <Typography variant="body1" component="span">
+              <Typography variant="body2" component="span">
                 (462 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h5" component="h3" marginTop={0}>
+                From $ 300
               </Typography>
             </Box>
           </Box>
